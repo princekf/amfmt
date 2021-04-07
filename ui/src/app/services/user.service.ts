de474e94db70cd099ec0a69b290fb4f134f7e14b
Prince Francis <princekfrancis@gmail.com>
2021-04-07 16:02:52 +0530

    Login and Register page style changes.

--- ui/src/app/services/user.service.ts
+++ ui/src/app/services/user.service.ts
@@ -1,3 +1,5 @@
+
+
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -5,13 +7,14 @@ import { UserS } from '@shared/entity/User';
 import { BASE_URI, USER_API, SIGNUP_API, LOGIN_API } from '@shared/serverAPI';
 import { catchError } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
-export class LoginSignupService {
+export class UserService {
 
   constructor(
-    private readonly http:HttpClient
+    private readonly http: HttpClient
   ) { }
 
   signUp(user: UserS): Observable<unknown> {
@@ -22,7 +25,7 @@ export class LoginSignupService {
 
   }
 
-  login(user: {email: string, password: string}): Observable<unknown> {
+  login(user: { email: string; password: string; }): Observable<unknown> {
 
     return this.http.post(`${BASE_URI}${USER_API}${LOGIN_API}`, user).pipe(
       catchError((err) => throwError(err))
